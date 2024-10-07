31057f3e9eecc21f5e437911a2ca036c46952dc5
Студенческая <@rbiu.local>
2024-10-07 11:09:37 +0500

    create project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import { Header } from "./components/header/Header";
-import { Set } from './components/set/Set';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Set />     
+ 
     </div>
   );
 }
--- src/components/header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import './Header.css'
-
-export function Header(){
-    return(
-        <div className="header">
-            <h1>Флэш-карточки</h1>
-        </div>
-    );
-}
