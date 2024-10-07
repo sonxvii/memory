0717cc786f1b0b77e5fc35e25ce3a3caf42eb741
JuliaMuk <mukhina_ur@mail.ru>
2024-10-07 09:25:51 +0500

    add control panel

--- src/components/Set.jsx
+++ src/components/Set.jsx
@@ -1,16 +1,31 @@
 import { useState } from "react";
-import {Card} from "./Card";
-export function Set(){
+import { Card } from "./Card";
+export function Set() {
     const cards = require('../data'); //подключить данные с data.json
-    const [step, setStep] =useState(0);
-    return(
+    const [step, setStep] = useState(0);
+    const handleNext = () => {
+        if (step < cards.length - 2) {
+            setStep(step + 1)
+        }
+    }
+    const handlePrev = () => {
+        if (step > 0) {
+            setStep(step - 1)
+        }
+    }
+    return (
         <div>
             <h2>Название набора</h2>
-            <Card 
-                front = { cards[step].front }
-                back = { cards[step].back } 
+            <Card
+                key={cards[step].id}
+                front={cards[step].front}
+                back={cards[step].back}
             />
-            <div>элементы управления</div>
+            <div className="control-panel">
+                <button disabled ={step == 0 ? true : false} onClick={handlePrev}> &lt; </button>
+                <p> {step+1} / {cards.length} </p>
+                <button disabled ={step == cards.length -1 ? true : false}onClick={handleNext}> &gt; </button>
+            </div>
         </div>
     );
 }
